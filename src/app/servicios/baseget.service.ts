2040a85f165be426e24946550dd077aa6fa76d35
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2022-11-29 13:53:20 -0600

    a la verga todo

--- src/app/servicios/baseget.service.ts
+++ src/app/servicios/baseget.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams, HttpRequest} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -14,12 +14,29 @@ export class BasegetService {
   ) { }
 
   consultarBases(): Observable<any> {
-    const url = "/base";
-    return this.http.get(url);
+    const httpOptions = {
+      headers : new HttpHeaders()
+    }
+
+    httpOptions.headers.append('Access-Control-Allow-Origin', '*');
+    httpOptions.headers.append('Content-Type', 'application/json');
+    httpOptions.headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+    const url = "http://3.214.137.245:5000/base";
+    return this.http.get(url, httpOptions);
   }
 
   agregarBase(dir : string): void{
-    const url = "/base";
-    this.http.post(url, {"direccion" : dir}, {responseType : 'json'});
+    const httpOptions = {
+      headers : new HttpHeaders()
+    }
+
+    httpOptions.headers.append('Access-Control-Allow-Origin', '*');
+    httpOptions.headers.append('Content-Type', 'application/json');
+    httpOptions.headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+
+    const url = "http://3.214.137.245:5000/base";
+    this.http.post(url, {"direccion" : dir}, httpOptions);
   }
 }
